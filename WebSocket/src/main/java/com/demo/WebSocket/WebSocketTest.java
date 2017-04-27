85733c8d53c3904e74668a18f4f6ea9ea04554a1
DELL <DELL@192.168.191.4>
2017-04-27 09:56:07 +0800

    加了几个方法和注解

--- WebSocket/src/main/java/com/demo/WebSocket/WebSocketTest.java
+++ WebSocket/src/main/java/com/demo/WebSocket/WebSocketTest.java
@@ -1,9 +1,18 @@
 package com.demo.WebSocket;
 
+import javax.management.MXBean;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-//访问这个webSocket的路径
+//访问这个webSocket的路径，并注解这个类是一个WebSocket类
+//注意：这个类必须要有个无参的默认构造函数
+//这种实现的webSocket的方式遵循标准 JSR356 规范实现
+//也有一种实现是Tomcat高版本自定义的api。
+//这里不解释另一种了
 @ServerEndpoint("/echo")
 public class WebSocketTest {
 	//与客户端连接的通道（会话）
@@ -13,12 +22,34 @@ public class WebSocketTest {
 	
 	
 	/**
-	 * 
+	 * 当webSocket连接打开时
 	 * @param session
 	 */
 	@OnOpen
 	public void onOpen(Session session){
 		
 	}
+	/**
+	 * 用于接受传入的客户端信息
+	 */
+	@OnMessage
+	public void onMessage(String message){
+		
+	}
+	/**
+	 * 当webSocket连接关闭时
+	 * @param session 连接的会话
+	 * @param reason 封装更多关于关闭的细节
+	 */
+	@OnClose
+	public void onClose(Session session, CloseReason reason){
+	}
+	/**
+	 * 不知道什么时候会调用，暂且留着
+	 * @param t
+	 */
+	@OnError
+	 public void onError(Throwable t) {
+	 }
 	
 }
--- WebSocket/src/main/webapp/index.jsp
+++ WebSocket/src/main/webapp/index.jsp
@@ -1,5 +1,22 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>WebSocket客户端</h2>
+<script type="text/javascript">
+	//新建一个WebSocket对象,socket的协议一般以ws开头，安全的协议以wss开头
+	var ws=new WebSocket("ws://webSocket/echo");
+	//
+	ws.onopen = function (){
+		ws.send("Test!");
+	};
+	ws.onmessage = function(evt){
+		console.log(evt.data);ws.close(200,"正常关闭");
+		}; 
+	ws.onclose = function(evt){
+			console.log("WebSocketClosed!");
+	}; 
+	ws.onerror = function(evt){
+		console.log("WebSocketError!");
+	};
+</script>
 </body>
 </html>
