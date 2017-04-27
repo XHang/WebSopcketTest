a2ccf5c3924b9e2758e3dd9884d82dca1ea47b71
Mr-hang <1083594261@qq.com>
2017-04-27 23:43:08 +0800

    //

--- WebSocket/src/main/java/com/demo/WebSocket/WebSocketTest.java
+++ WebSocket/src/main/java/com/demo/WebSocket/WebSocketTest.java
@@ -1,8 +1,12 @@
 package com.demo.WebSocket;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.management.MXBean;
 import javax.websocket.CloseReason;
@@ -21,7 +25,9 @@ import javax.websocket.server.ServerEndpoint;
 public class WebSocketTest {
 	//与客户端连接的通道（会话）
 	//在线用户个数
-	private  List<Session> userList=new ArrayList<Session>();
+	private  Map<String,WebSocketTest> users=new HashMap<String,WebSocketTest>();
+	
+	Session session;
 	/**
 	 * 当webSocket连接打开时
 	 * @param session
@@ -29,17 +35,21 @@ public class WebSocketTest {
 	@OnOpen
 	public void onOpen(Session session){
 		System.out.println("有新的会话加入！它的id为："+session.getId());
-		userList.add(session);
-		if(userList.size()!=1){
-			pushMsg();
-		}
+		String nickName=session.getQueryString();
+//		if(users.size()!=0){
+//			pushMsg(nickName);
+//		}
+		users.put(nickName, this);
+		System.out.println("用户"+nickName+"上线了");
+		
 	}
 	/**
 	 * 用于接受传入的客户端信息
 	 */
 	@OnMessage
-	public void onMessage(String message){
-		System.out.println("客户端说："+message);
+	public void onMessage(String message,Session session){
+//		String tagerUser=session.getQueryString();
+//		String sourceUser=users.
 	}
 	/**
 	 * 当webSocket连接关闭时
@@ -58,12 +68,24 @@ public class WebSocketTest {
 	 public void onError(Throwable t) {
 		System.out.println("错误啦，原因是......"+t.getMessage());
 	 }
-	//推送上线通知
-	public void pushMsg(){
-		//除了新上线的用户，其他用户都要接受到通知
-		for(int i=0;i<userList.size()-2;i++){
-//			userList.get(i).
+	//推送某人上线通知
+	public void pushMsg(String nickName){
+		Set<Map.Entry<String, WebSocketTest>> entry=users.entrySet();
+		try{
+			for(Iterator<Map.Entry<String, WebSocketTest>> it=entry.iterator();it.hasNext();){
+				Map.Entry<String, WebSocketTest> user=it.next();
+				user.getValue().session.getBasicRemote().sendText(nickName+"上线了");
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
 		}
 	} 
+	/**
+	 * 通过session来取出它的昵称
+	 */
+	private void getUserNickNameBySession(){
+		
+	}
 	
 }
